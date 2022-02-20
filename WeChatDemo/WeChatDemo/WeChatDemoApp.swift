aa7fb7b5c2623d805984705e7d8b51551b29de3e
tianjing09 <352444797@qq.com>
2022-02-20 16:27:56 +0800

    Add files via upload
    demo like wechat

---
+++ WeChatDemo/WeChatDemo/WeChatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeChatDemoApp.swift
+//  WeChatDemo
+//
+//  Created by jing 田 on 2022/2/20.
+//
+
+import SwiftUI
+
+@main
+struct WeChatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
